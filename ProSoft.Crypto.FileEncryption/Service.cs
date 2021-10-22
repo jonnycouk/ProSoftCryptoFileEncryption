61d0ca1ed3d27e64a9336218144408de9a95b46b
Jonny <jonny@jonny.uk>
2021-10-22 19:50:02 +0100

    Initial Commit

---
+++ ProSoft.Crypto.FileEncryption/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProSoft.Crypto.FileEncryption
+{
+    public class Service
+    {
+        public static bool EncryptFile(string sourceFile, string destinationFile, string password)
+        {
+            SharpAESCrypt.SharpAESCrypt.Encrypt(password, sourceFile, destinationFile);
+            return System.IO.File.Exists(destinationFile);
+        }
+
+        public static bool DecryptFile(string sourceFile, string destinationFile, string password, out string passwordStatus)
+        {
+            try
+            {
+                SharpAESCrypt.SharpAESCrypt.Decrypt(password, sourceFile, destinationFile);
+                passwordStatus = "OK";
+                return System.IO.File.Exists(destinationFile);
+            }
+            catch (Exception e)
+            {
+                passwordStatus = "INCORRECT_PASSWORD";
+            }
+
+            return false;
+        }
+    }
+}
